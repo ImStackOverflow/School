2eeca0a162fc54736f00294bea4cd62f6e9f29dc
Gavin Chen <ghchen@ucsc.edu>
2018-01-23 15:37:41 -0800

    got fucking get to work VERY BASIC FUCK ME

--- cs128/hw1.py
+++ cs128/hw1.py
@@ -1,45 +1,23 @@
 from flask import Flask
-import os
-import socket
+from flask import request
 
 app = Flask(__name__)
 
-tasks = [
-    {
-        'id' : ,
-        'description' : u'Hello User!',
-        'done' : False
-    },
-    {
-        'id' : hello?name=John,
-        'description' : u'Hello John!',
-        'done' : False
-    },
-    {
-        'id' : 3,
-        'description' : u'This is a GET request',
-        'done' : False
-    },
-    {
-        'id' : 4,
-        'description' : u'This is a POST request',
-        'done' : False
-    }
-]
+@app.route('/hello', methods=['GET'])
+def get_shit():
+    #username = request.args.get('name')
+    #if not username :
+    #    username = 'User'
+    return 'Hello john !'
 
-
-@app.route('/hello<string:task_id>', methods=['GET'])
-def get_shit(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    return task
-
-@app.route('/check', methods=['GET'])
+"""@app.route('/check', methods=['GET'])
 def getCheck:
-    return tasks[2]
+    task = task[
+    return tasks['description'], status.HTTP_200_OK
 
 @app.route('/check', methods=['POST'])
 def postCheck
     return tasks[3]
-
+"""
 if __name__ == '__main__':
-    app.run()
+    app.run(host = '0.0.0.0', port=81)
