f91292905f72e9b4c0b6955001a4d84229e626a5
Gavin Chen <ghchen@ucsc.edu>
2018-01-23 23:34:09 -0800

    think i got this shit

--- cs128/hw1.py
+++ cs128/hw1.py
@@ -2,22 +2,27 @@ from flask import Flask
 from flask import request
 
 app = Flask(__name__)
+try: 
+	@app.route('/hello', methods=['GET'])
+	def get_shit():
+	    username = request.args.get('name')
+	    if not username :
+	       username = 'User'
+	    return 'Hello ' + username + '!'
 
-@app.route('/hello', methods=['GET'])
-def get_shit():
-    username = request.args.get('name')
-    if not username :
-       username = 'User'
-    return username
+	@app.route('/check', methods = ['GET', 'POST'])
+	def method(): 
+		
+		if request.method == 'POST':
+		    return 'This is a POST request', 200
+		elif request.method == 'GET':
+		    return 'This is a GET request', 200
+		else: 
+		    return 405
 
-app.route('/check', methods = ['GET', 'POST'])
-def method(): 
-	
-	if request.method == 'POST':
-	    return 'This is a POST request', 200
-	elif request.method == 'GET':
-	    return 'This is a GET request', 200
-	else: 
+except:
+	@app.route('/')
+	def error():
 	    return 405
 
 if __name__ == '__main__':
